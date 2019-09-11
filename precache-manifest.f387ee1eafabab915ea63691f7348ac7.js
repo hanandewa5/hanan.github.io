96b6a17193746f18d40797c49a484041328abc00
hanan <hanandewa5@gmail.com>
2019-09-11 18:00:52 +0700

    Updates

---
+++ precache-manifest.f387ee1eafabab915ea63691f7348ac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c99ebce29b3be4900039",
+    "url": "/portofolio/static/css/main.41546a4d.chunk.css"
+  },
+  {
+    "revision": "c99ebce29b3be4900039",
+    "url": "/portofolio/static/js/main.c99ebce2.chunk.js"
+  },
+  {
+    "revision": "591664a1c0dde4f94f41",
+    "url": "/portofolio/static/js/1.591664a1.chunk.js"
+  },
+  {
+    "revision": "f9ab645a81c206d1f2da",
+    "url": "/portofolio/static/js/runtime~main.f9ab645a.js"
+  },
+  {
+    "revision": "b6f0103df6dc49ee26c8dd50be2b674d",
+    "url": "/portofolio/index.html"
+  }
+];
